f2b7ed298aff266e8bdc4ef090a9c6c266890b05
Pierre_T <pierre.tsiakkaros@gmail.com>
2025-02-07 02:00:41 +0100

    a propos 1

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,39 @@ const blog = defineCollection({
   }),
 });
 
+const pages = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    intro: z.string(),
+    mission: z.object({
+      title: z.string(),
+      description: z.string(),
+      objectives: z.array(z.string())
+    }),
+    values: z.array(z.object({
+      title: z.string(),
+      description: z.string()
+    })),
+    approach: z.object({
+      title: z.string(),
+      description: z.string(),
+      steps: z.array(z.object({
+        step: z.string(),
+        title: z.string(),
+        description: z.string()
+      }))
+    }),
+    cta: z.object({
+      title: z.string(),
+      description: z.string()
+    })
+  })
+});
+
 export const collections = {
   blog,
+  pages,
 };
